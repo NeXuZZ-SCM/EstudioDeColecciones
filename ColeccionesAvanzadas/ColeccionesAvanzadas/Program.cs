fc4c8b42fe38d8c3587186765efaaba209cab9c2
nexuzz <nexuzz@outlook.com>
2019-01-09 00:59:29 -0300

    Carga primigenia practica

---
+++ ColeccionesAvanzadas/ColeccionesAvanzadas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColeccionesAvanzadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<Cliente> ejemplar = new Queue<Cliente>();
+        }
+    }
+    public class Cliente
+    {
+        public string Nombre{ get; set; }
+        public int Edad { get; set; }
+    }
+}
